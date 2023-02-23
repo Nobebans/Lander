627bc57edfdcb20fe545ac39024d1834e69d4237
Nobebans <isakp@live.se>
2023-02-23 13:38:29 +0100

    upgf 10

--- Program.cs
+++ Program.cs
@@ -5,13 +5,26 @@
         public class Land
         {
             public string namn, styretyp, huvudstad, invånarantal;
+
+            public void Print()
+            {
+                Console.WriteLine($"land: {namn}");
+                Console.WriteLine($"styretyp: {styretyp}");
+                Console.WriteLine($"huvudstad: {huvudstad}");
+                Console.WriteLine($"invånarantal: {invånarantal}");
+                Console.WriteLine();
+            }
         }
 
         static void Main(string[] args)
         {
-            Land Sverige = new Land() { styretyp = "monarki", huvudstad = "Stockholm", invånarantal = "10512820" };
-            Land Tyskland = new Land() { styretyp = "republik", huvudstad = "Berlin", invånarantal = "83783902" };
-            Land SanMarino = new Land() { styretyp = "republik", huvudstad = "San Marino", invånarantal = "33600" };
+            Land sverige = new Land() { namn = "Sverige", styretyp = "monarki", huvudstad = "Stockholm", invånarantal = "10512820" };
+            Land tyskland = new Land() { namn = "Tyskland", styretyp = "republik", huvudstad = "Berlin", invånarantal = "83783902" };
+            Land sanMarino = new Land() { namn = "San Marino", styretyp = "republik", huvudstad = "San Marino", invånarantal = "33600" };
+
+            sverige.Print();
+            tyskland.Print();
+            sanMarino.Print();
         }
     }
 }
