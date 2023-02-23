c448bace2b3ab78a779c42eda3ada641dbd09df5
Nobebans <isakp@live.se>
2023-02-23 13:25:24 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Länder
+{
+    internal class Program
+    {
+        public class Land
+        {
+            public string namn, styretyp, huvudstad, invånarantal;
+        }
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
