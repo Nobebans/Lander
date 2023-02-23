69a153ba2a3fea8fc2a092f4040d6f3227299432
Nobebans <isakp@live.se>
2023-02-23 13:30:30 +0100

    Upgft 9

--- Program.cs
+++ Program.cs
@@ -9,7 +9,9 @@
 
         static void Main(string[] args)
         {
-            
+            Land Sverige = new Land() { styretyp = "monarki", huvudstad = "Stockholm", invånarantal = "10512820" };
+            Land Tyskland = new Land() { styretyp = "republik", huvudstad = "Berlin", invånarantal = "83783902" };
+            Land SanMarino = new Land() { styretyp = "republik", huvudstad = "San Marino", invånarantal = "33600" };
         }
     }
 }
